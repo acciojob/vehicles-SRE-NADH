af93594fac1048dbdb4d345f221fc03918017566
SRE-NADH <sreenadh669@gmail.com>
2023-04-18 13:04:03 +0530

    bugfix

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -18,6 +18,7 @@ public class Car extends Vehicle {
         this.isManual= isManual;
         this.type=type;
         this.seats=seats;
+        this.currentGear=1;
     }
 
     public void changeGear(int newGear){
@@ -56,5 +57,33 @@ public class Car extends Vehicle {
 
     public int getSeats() {
         return seats;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public void setManual(boolean manual) {
+        isManual = manual;
+    }
+
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
     }
 }
